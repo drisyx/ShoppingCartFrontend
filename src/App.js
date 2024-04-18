1588ca49911d2ce5e36a589e8db45e9fceb09dcc
drisyx <drisyashenoyd@gmail.com>
2024-04-18 21:50:06 +0530

    viewall created

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import logo from './logo.svg';
 import './App.css';
 import AddProduct from './components/AddProduct';
 import SearchProduct from './components/SearchProduct';
+import ViewAll from './components/ViewAll';
 
 function App() {
   return (
     <div>
       <AddProduct/>
       <SearchProduct/>
+     <ViewAll/>
     </div>
   );
 }
